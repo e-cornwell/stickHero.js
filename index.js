cf83a41a26d9280387e1f28963e364222b918f8b
Ethan Cornwell <ethan.aubrey22@gmail.com>
2024-01-08 12:59:03 -0700

    draw hero and platform functions

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const canvasWidth = 375;
 const canvasHeight = 375;
 const platformHeight = 100;
 
-
 //UI Elements
 const scoreElement = document.getElementById("score");
 const restartButton = document.getElementById("restart");
@@ -33,70 +32,103 @@ resetGame();
 
 //Reset gamestate and layout
 function resetGame() {
-    // Reset game state
-    phase = "waiting";
-    lastTimestamp = undefined;
+  // Reset game state
+  phase = "waiting";
+  lastTimestamp = undefined;
 
-    //First platform always the same
-    platforms = [{ x: 50, w: 50 }];
-    generatePlatform();
-    generatePlatform();
-    generatePlatform();
-    generatePlatform();
+  //First platform always the same
+  platforms = [{ x: 50, w: 50 }];
+  generatePlatform();
+  generatePlatform();
+  generatePlatform();
+  generatePlatform();
 
-    //Init hero position
-    heroX = platforms[0].x + platforms[0].w - 30; //Hero starts near edge 
-    heroY = 0;
+  //Init hero position
+  heroX = platforms[0].x + platforms[0].w - 30; //Hero starts near edge
+  heroY = 0;
 
-    //How much the screen shifts back
-    sceneOffset = 0;
+  //How much the screen shifts back
+  sceneOffset = 0;
 
-    //Always a stick, even when invisible.
-    sticks = [{ x: platforms[0].x + platforms[0].w, length: 0, rotation: 0 }];
+  //Always a stick, even when invisible.
+  sticks = [{ x: platforms[0].x + platforms[0].w, length: 0, rotation: 0 }];
 
-    //Score
-    score = 0;
+  //Score
+  score = 0;
 
-    //Reset UI
-    restartButton.style.display = "none"; //Hide reset button
-    scoreElement.innerText = score; // Reset score display
+  //Reset UI
+  restartButton.style.display = "none"; //Hide reset button
+  scoreElement.innerText = score; // Reset score display
 
-    draw();
-};
+  draw();
+}
 
 function generatePlatform() {
-    const minimumGap = 40;
-    const maximumGap = 200;
-    const minimumWidth = 20;
-    const maximumWidth = 100;
+  const minimumGap = 40;
+  const maximumGap = 200;
+  const minimumWidth = 20;
+  const maximumWidth = 100;
 
-    //X coordinate of the right edge of the furthest platform
-    const lastPlatform = platforms[platforms.length - 1 ];
-    let furthestX = lastPlatform.x + lastPlatform.w;
+  //X coordinate of the right edge of the furthest platform
+  const lastPlatform = platforms[platforms.length - 1];
+  let furthestX = lastPlatform.x + lastPlatform.w;
 
-    const x = 
-        furthestX +
-        minimumGap +
-        Math.floor(Math.random() * (maximumGap - minimumGap));
-    const w = 
-        minimumWidth + Math.floor(Math.random() * (maximumWidth - minimumWidth));
-    platforms.push({x, w});
-};
+  const x =
+    furthestX +
+    minimumGap +
+    Math.floor(Math.random() * (maximumGap - minimumGap));
+  const w =
+    minimumWidth + Math.floor(Math.random() * (maximumWidth - minimumWidth));
+  platforms.push({ x, w });
+}
 
 draw();
 
 function draw() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  let platforms = [
+    { x: 50, w: 50 },
+    { x: 90, w: 30 },
+  ];
 
-};
+  function drawPlatforms() {
+    platforms.forEach(({ x, w }) => {
+        //Draw platform
+        ctx.fillStyle = "black";
+        ctx.fillRect(x, canvasHeight - platformHeight, w, platformHeight);
+    });
+  };
 
-window.addEventListener("mousedown", function(event) {
+  function drawHero() {
+    const heroWidth = 20;
+    const heroHeight = 30;
 
-});
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+        heroX,
+        heroY + canvasHeight - platformHeight - heroHeight,
+        heroWidth,
+        heroHeight
+    );
+  };
 
-window.addEventListener("mouseup", function(event) {
+  //Save the current transformation
+  ctx.save();
 
-});
+  //Shifting the view
+  ctx.translate(-sceneOffset, 0);
 
-function animate(timeStamp) {
+  //Draw scene
+  drawPlatforms();
+  drawHero();
+  drawSticks();
 
-};
+  //Restore transformation to the last save
+  ctx.restore();
+}
+
+window.addEventListener("mousedown", function (event) {});
+
+window.addEventListener("mouseup", function (event) {});
+
+function animate(timeStamp) {}
